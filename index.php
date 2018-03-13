d8a738dd71e657a00cb52b9481e499fdd33c3fcf
jatinder-pal <boskim.3ginfio@gmail.com>
2018-03-13 18:17:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php 
-echo "hello12";
+echo "hello123";
 $url = "https://9dd7af7202b9e8c10102d3bf486e1bc3:c249c83bc4641859f72478bae65866ea@wishaddict.myshopify.com/admin/orders.json";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data= curl_exec($ch);
 curl_close($ch);
 $data=json_decode($data, true);
-echo "<pre>";print_r($data);echo "</pre>";
+//echo "<pre>";print_r($data);echo "</pre>";
 if($data !=''){
 	foreach($data as $data1){
-		echo $data1['orders']['id'];
+		echo "id=".['id'];
 	/*$ch = curl_init("https://3b4fa03fc3c62dd4bc12df85201806de:826c019d2d1d00f99b3d90682ad58851@jai-shri-ram-2.myshopify.com/admin/orders.json");
 	
 			$order = array(
