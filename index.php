b369c0cde7265873e71ba996e76b6c70f0d568f9
jatinder-pal <boskim.3ginfio@gmail.com>
2018-03-13 19:02:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,15 +11,18 @@ if($data !=''){
 	foreach($data['orders'] as $data1){
       $order_id=$data1['id'];
 	$ch = curl_init("https://3b4fa03fc3c62dd4bc12df85201806de:826c019d2d1d00f99b3d90682ad58851@jai-shri-ram-2.myshopify.com/admin/orders.json");
-	
+	$line_items=array(
+				array(
+					'title' =>'Elegant Engagement Ring',
+					'price' => 7.94,
+					'quantity' => 1,
+					'variant_id' => 255841435662
+					
+				),
+				 
+			);
 			$order = array(
-				"line_items": [
-				      {
-					"variant_id":255841435662,
-					"quantity": 1
-				      }
-				    ]
-				  }
+				'line_items' => $line_items,
 				'transactions' => array(
 					array(
 						'kind' => "authorization",
