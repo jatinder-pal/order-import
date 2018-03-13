957c9142b76956c71ffc59a36d8b62a66107e003
jatinder-pal <boskim.3ginfio@gmail.com>
2018-03-13 16:28:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ echo "hello";
 $url = "https://3b4fa03fc3c62dd4bc12df85201806de:826c019d2d1d00f99b3d90682ad58851@jai-shri-ram-2.myshopify.com/admin/orders.json";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$var= curl_exec($ch);
+$data= curl_exec($ch);
 curl_close($ch);
-echo "<pre>";print_r($var);echo "</pre>";
+$data=json_decode($data, true);
+echo "<pre>";print_r($data);echo "</pre>";
 ?>
