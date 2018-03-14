ab40b1ee9391778c15cfcc51cc361cfb89b530f5
jatinder-pal <boskim.3ginfio@gmail.com>
2018-03-14 13:09:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-echo "hello13";
+echo "hello123";
 $url = "https://9dd7af7202b9e8c10102d3bf486e1bc3:c249c83bc4641859f72478bae65866ea@wishaddict.myshopify.com/admin/orders.json";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -22,7 +22,7 @@ if($data !=''){
 					'title' =>$line_items['title'],
 					'price' => $line_items['price'],
 					'quantity' =>$line_items['quantity'],
-					'variant_id' => $line_items['variant_id']
+					'variant_id' => 7149643333679
 					
 				);
 		$line_count++;		
